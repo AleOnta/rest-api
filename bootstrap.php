a0ac77bd129f9abbd30de037c75878dbdc2a73f3
AleOnta <casualotn@gmail.com>
2025-06-01 14:34:39 +0200

    initialized db connection in bootstrap file of the app

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,11 @@
 <?php
-
 require "vendor/autoload.php";
+
+use Src\System\DB;
 
 # initialize and read .env into memory
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+# get db connection
+$db = new DB()->getConnection();
