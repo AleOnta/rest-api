c1c009237181f85270c23df723d9124cfc283dbc
AleOnta <casualotn@gmail.com>
2025-06-01 16:07:07 +0200

    added missing attr & getters/setters to User model

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,6 +2,9 @@
 
 namespace Src\Models;
 
+use DateTime;
+use DateTimeZone;
+
 class User
 {
 
@@ -9,13 +12,17 @@ class User
     protected string $email;
     protected string $username;
     protected string $password;
+    protected string $createdAt;
+    protected string $updatedAt;
 
-    protected function __construct(?int $id, string $email, string $username, string $password)
+    protected function __construct(?int $id, string $email, string $username, string $password, string $createdAt, string $updatedAt)
     {
         $this->id = $id;
         $this->email = $email;
         $this->username = $username;
         $this->password = $password;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
     }
 
     public static function new(string $email, string $username, string $password)
@@ -24,7 +31,9 @@ class User
             null,
             $email,
             $username,
-            $password
+            $password,
+            new DateTime('now', new DateTimeZone('UTC'))->format('Y-m-d H:i:s'),
+            new DateTime('now', new DateTimeZone('UTC'))->format('Y-m-d H:i:s')
         );
     }
 
@@ -34,7 +43,9 @@ class User
             $data['id'],
             $data['email'],
             $data['username'],
-            $data['password']
+            $data['password'],
+            $data['created_at'],
+            $data['updated_at']
         );
     }
 
@@ -56,5 +67,30 @@ class User
     public function getPassword()
     {
         return $this->password;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
     }
 }
