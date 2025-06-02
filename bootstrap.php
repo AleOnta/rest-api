511ece3440cd3437871ae0f6feebb3cfdf5d9226
AleOnta <casualotn@gmail.com>
2025-06-02 15:01:01 +0200

    initialized and implemented Global Exception handler at application start

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,11 @@
 <?php
 require "vendor/autoload.php";
 
+$exceptionHandler = include 'src/Exceptions/Handler.php';
+set_exception_handler($exceptionHandler);
+
+use Src\Gateways\UserGateway;
+use Src\Models\User;
 use Src\System\DB;
 
 # initialize and read .env into memory
