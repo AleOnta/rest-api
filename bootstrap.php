8e1cd454eee2cadeeb4af40cde4dab2b96275ee4
AleOnta <casualotn@gmail.com>
2025-06-02 20:38:37 +0200

    minor changes

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,10 @@
 <?php
 require "vendor/autoload.php";
 
+# set up global exception handler for application
 $exceptionHandler = include 'src/Exceptions/Handler.php';
 set_exception_handler($exceptionHandler);
-
-use Src\Gateways\UserGateway;
-use Src\Models\User;
-use Src\System\DB;
 
 # initialize and read .env into memory
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-# get db connection
-$db = new DB()->getConnection();
