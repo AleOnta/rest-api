e25fde07e7653aa73de21c7d0fbb422a6a5f66cb
AleOnta <casualotn@gmail.com>
2025-06-02 15:00:20 +0200

    adds Global exception handler

---
+++ src/Exceptions/Handler.php
@@ -0,0 +1,48 @@
+<?php
+
+use Src\Exceptions\AlreadyExistsException;
+use Src\Exceptions\ValidationException;
+
+return function (Throwable $e) {
+
+    $data = [
+        'code' => 500,
+        'message' => '...'
+    ];
+
+    switch ($e) {
+
+        # ALREADY EXISTS
+        case $e instanceof AlreadyExistsException:
+            $data['code'] = 409;
+            $data['message'] = $e->buildMessage();
+            break;
+
+        # VALIDATION ERROR
+        case $e instanceof ValidationException:
+            $data['code'] = 422;
+            $data['message'] = $e->getMessage();
+            $data['errors'] = $e->getErrors();
+            break;
+
+        # DEF FALLBACK
+        default:
+            $data['message'] = $_ENV['APP_ENV'] === 'dev'
+                ? $e->getMessage()
+                : 'An error has occurred, try again later...';
+            break;
+    }
+
+    $response = [
+        'error' => true,
+        'message' => $data['message']
+    ];
+
+    if (isset($data['errors'])) {
+        $response['errors'] = $data['errors'];
+    }
+
+    http_response_code($data['code']);
+    echo json_encode($response);
+    exit;
+};
