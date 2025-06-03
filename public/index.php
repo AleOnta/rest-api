57817477f19e51d898df25b46c974fe3a45dd66d
AleOnta <casualotn@gmail.com>
2025-06-03 23:23:45 +0200

    adds 2 simple users routes to the API

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
 <?php
+
+require "../bootstrap.php";
+
+use Src\System\Router;
+use Src\Controllers\UserController;
+
+# create the router class
+$router = new Router();
+
+# register routes
+# 1. /users...
+$router->group('/users', function ($router) {
+    $router->get('/me', UserController::class, 'index');
+    $router->get('/{id}', UserController::class, 'show');
+});
+
+$router->match();
