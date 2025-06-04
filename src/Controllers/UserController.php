946d8556e8616e826517a62eb4b41a1861a9d643
AleOnta <casualotn@gmail.com>
2025-06-04 22:30:33 +0200

    implemented authorization in user controller and adds php docs

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -5,9 +5,11 @@ namespace Src\Controllers;
 use Src\Gateways\UserGateway;
 use Src\System\DB;
 use Src\Traits\AuthenticatesRequest;
+use Src\Traits\AuthorizeRequest;
 
 class UserController
 {
+    use AuthorizeRequest;
     use AuthenticatesRequest;
     private UserGateway $userGateway;
 
@@ -17,6 +19,12 @@ class UserController
         $this->userGateway = new UserGateway($db);
     }
 
+    /**
+     * Retrieve and returns data the authenticated user. </br>
+     * This endpoint requires authentication.
+     * @return json <p>a json response containing the authenticated user data</p>
+     * @throws AuthenticationException
+     */
     public function index()
     {
         # auth
@@ -25,21 +33,35 @@ class UserController
         $this->response(['success' => true, 'data' => ['user' => $user->toArray()]]);
     }
 
+    /**
+     * Retrieve and returns data of a user by its id. </br>
+     * This endpoint requires admin authorization.
+     * @param int $id <p>the id of the user to retrieve</p>
+     * @return json <p>a json response containing the required resource or an empty array</p>
+     * @throws AuthorizationException
+     * @throws AuthenticationException
+     */
     public function show(int $id)
     {
-        # auth
+        # authentication
         $auth = $this->authenticate();
-        # proceed with request
+        # authorization
+        $userId = $this->authorize($auth->getId());
+        # retrieve data
         $user = $this->userGateway->findById($id);
-        if ($user) {
-            $user->getId() === $auth->getId()
-                ? $this->response(['success' => true, 'data' => ['user' => $user->toArray()]])
-                : $this->unauthorized();
-        }
-        $this->response(['success' => true, 'data' => []]);
+        # response
+        $user
+            ? $this->response(['success' => true, 'data' => ['user' => $user->toArray()]])
+            : $this->response(['success' => true, 'data' => []]);
     }
 
-    public function response($data, $code = 200)
+    /**
+     * Helper function that return a response to the client.
+     * @param array $data <p>the payload to return to the client</p>
+     * @param int $code <p>the http response code attached to the response</p>
+     * @return json <p>a json response containing the required resource or an empty array</p>
+     */
+    public function response(array $data, int $code = 200)
     {
         http_response_code($code);
         echo json_encode($data, JSON_PRETTY_PRINT);
