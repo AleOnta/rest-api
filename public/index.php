ff898abae328e975a3102932f74f50fede333bcc
AleOnta <casualotn@gmail.com>
2025-06-14 14:16:13 +0200

    adds user PATCH edit endpoint

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ $router->group('/users', function ($router) {
     $router->get('/me', UserController::class, 'index');
     $router->get('/{id}', UserController::class, 'show');
     $router->post('/', UserController::class, 'register');
-    $router->patch('/{id}', UserController::class, 'update');
+    $router->patch('/{id}', UserController::class, 'edit');
 });
 
 $router->match();
